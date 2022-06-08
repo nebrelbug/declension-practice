2a8522ae1546bbb6a16317c12cc35bda05050c9e
bgub <benjamin.gubler@missionary.org>
2022-06-08 13:09:17 +0200

    3 files modified

--- pages/slovak.tsx
+++ pages/slovak.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/home.module.css';
-
+import { useState } from 'react';
 import { generateSimpleSentences } from '../src/sentence-generators';
+import { useTweaks } from 'use-tweaks';
 
 let res = generateSimpleSentences();
 
@@ -17,11 +18,26 @@ function arrayLoop(arr) {
 }
 
 export default function Home() {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  const config = useTweaks('Spring', {
+    A: { value: 0, min: 0, max: 1 },
+    mass: { value: 1, min: 1, max: 10 },
+    tension: { value: 170, min: 1, max: 200 },
+    friction: { value: 26, min: 1, max: 30 },
+  });
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Decline | Slovak</title>
       </Head>
+
+      <Checkbox label="My Value" value={checked} onChange={handleChange} />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -39,3 +55,12 @@ export default function Home() {
     </div>
   );
 }
+
+const Checkbox = ({ label, value, onChange }) => {
+  return (
+    <label>
+      <input type="checkbox" checked={value} onChange={onChange} />
+      {label}
+    </label>
+  );
+};
