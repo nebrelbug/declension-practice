b0fd2b48058c5aa6cb1375db0dd27bc337d9d62f
bgub <benjamin.gubler@missionary.org>
2022-06-19 13:25:19 +0200

    1 file modified

--- src/slovak/neuter.ts
+++ src/slovak/neuter.ts
@@ -1,93 +1,96 @@
 import { createNounDeclension } from '../utilities';
 
-export let mesto = createNounDeclension(
-  'n',
-  [
-    ['mesto', 'mesta', 'mestu', 'mesto', false, 'meste', 'mestom'],
-    ['mestá', 'miest', 'mestám', 'mestá', false, 'mestách', 'mestami'],
-  ],
-  'city',
-  'cities'
-);
-
-export let pole = createNounDeclension(
-  'n',
-  [
-    ['pole', 'poľa', 'poľu', 'pole', false, 'poli', 'poľom'],
-    ['polia', 'polí', 'poliam', 'polia', false, 'poliach', 'poľami'],
-  ],
-  'field',
-  'fields'
-);
-
-export let dievča = createNounDeclension(
-  'n',
-  [
+export let neuterNouns = [
+  createNounDeclension(
+    'n',
+    'essential',
     [
-      'dievča',
-      'dievčaťa',
-      'dievčaťu',
-      'dievča',
-      false,
-      'dievčati',
-      'dievčaťom',
+      ['mesto', 'mesta', 'mestu', 'mesto', false, 'meste', 'mestom'],
+      ['mestá', 'miest', 'mestám', 'mestá', false, 'mestách', 'mestami'],
     ],
+    'city',
+    'cities'
+  ),
+  createNounDeclension(
+    'n',
+    'essential',
     [
-      'dievčatá',
-      'dievčat',
-      'dievčatám',
-      'dievčatá',
-      false,
-      'dievčatách',
-      'dievčatami',
+      ['pole', 'poľa', 'poľu', 'pole', false, 'poli', 'poľom'],
+      ['polia', 'polí', 'poliam', 'polia', false, 'poliach', 'poľami'],
     ],
-  ],
-  'girl',
-  'girls'
-);
-
-export let námestie = createNounDeclension(
-  'n',
-  [
+    'field',
+    'fields'
+  ),
+  createNounDeclension(
+    'n',
+    'advanced',
     [
-      'námestie',
-      'námestia',
-      'námestiu',
-      'námestie',
-      false,
-      'námestí',
-      'námestím',
+      [
+        'dievča',
+        'dievčaťa',
+        'dievčaťu',
+        'dievča',
+        false,
+        'dievčati',
+        'dievčaťom',
+      ],
+      [
+        'dievčatá',
+        'dievčat',
+        'dievčatám',
+        'dievčatá',
+        false,
+        'dievčatách',
+        'dievčatami',
+      ],
     ],
+    'girl',
+    'girls'
+  ),
+  createNounDeclension(
+    'n',
+    'essential',
     [
-      'námestia',
-      'námestí',
-      'námestiam',
-      'námestia',
-      false,
-      'námestiach',
-      'námestiami',
+      [
+        'námestie',
+        'námestia',
+        'námestiu',
+        'námestie',
+        false,
+        'námestí',
+        'námestím',
+      ],
+      [
+        'námestia',
+        'námestí',
+        'námestiam',
+        'námestia',
+        false,
+        'námestiach',
+        'námestiami',
+      ],
     ],
-  ],
-  'town square',
-  'town square'
-);
-
-export let jablko = createNounDeclension(
-  'n',
-  [
-    ['jablko', 'jablka', 'jablku', 'jablko', false, 'jablku', 'jablkom'],
-    ['jablká', 'jabĺk', 'jablkám', 'jablká', false, 'jablkách', 'jablkami'],
-  ],
-  'apple',
-  'apples'
-);
-
-export let múzeum = createNounDeclension(
-  'n',
-  [
-    ['múzeum', 'múzea', 'múzeu', 'múzeum', false, 'múzeu', 'múzeom'],
-    ['múzeá', 'múzeí', 'múzeám', 'múzeá', false, 'múzeách', 'múzeami'],
-  ],
-  'museum',
-  'museums'
-);
+    'town square',
+    'town square'
+  ),
+  createNounDeclension(
+    'n',
+    'advanced',
+    [
+      ['jablko', 'jablka', 'jablku', 'jablko', false, 'jablku', 'jablkom'],
+      ['jablká', 'jabĺk', 'jablkám', 'jablká', false, 'jablkách', 'jablkami'],
+    ],
+    'apple',
+    'apples'
+  ),
+  createNounDeclension(
+    'n',
+    'advanced',
+    [
+      ['múzeum', 'múzea', 'múzeu', 'múzeum', false, 'múzeu', 'múzeom'],
+      ['múzeá', 'múzeí', 'múzeám', 'múzeá', false, 'múzeách', 'múzeami'],
+    ],
+    'museum',
+    'museums'
+  ),
+];
