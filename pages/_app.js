bc2500588b95d01b8fa4b562131e786d0a6f2448
Ben Gubler <nebrelbug@gmail.com>
2023-02-07 16:54:10 -0700

    GA4 migration

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,29 @@
-import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
+import "../styles/globals.css"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+// import { Analytics } from '@vercel/analytics/react';
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url
+      })
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
-      <Analytics />
+      {/* <Analytics /> */}
       <Component {...pageProps} />
     </>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
